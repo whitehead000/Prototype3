6fa734e137cbe8566cb7a8d36bb88531a9f460dd
whitehead <sota.takase@gmail.com>
2022-10-29 14:43:59 +0900

    [add] index

---
+++ SpawnManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private PlayerController playerControllerScript;
+
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            int obstacleIndex = Random.Range(0, obstaclePrefab.Length);
+            Instantiate(obstaclePrefab[obstacleIndex], spawnPos, obstaclePrefab[obstacleIndex].transform.rotation);
+            
+        }
+      
+    }
+}
