7d20d7597737e346b8b0d64917c982136be92a96
whitehead <sota.takase@gmail.com>
2022-10-29 14:32:54 +0900

    prototype1 commit

---
+++ CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject mainCamera;
+    private GameObject subCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("MainCamera");
+        subCamera = GameObject.Find("SubCamera");
+
+        subCamera.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mainCamera.SetActive(!mainCamera.activeSelf);
+            subCamera.SetActive(!subCamera.activeSelf);
+        }
+        
+    }
+
+    
+
+}
